b176b63c6c3aeee5cbef950765d93e0d207fdee7
victorguirra <victor.hugo.guirra@gmail.com>
2020-07-09 13:51:42 -0300

    Config Href

--- import.php
+++ import.php
@@ -9,13 +9,13 @@ while($data = mysqli_fetch_array($result)){
 ?>  
     <tr>
 
-    <td><?php echo $data['name'] ?></td>
-    <td><?php echo $data['age'] ?></td>
-    <td><?php echo $data['email'] ?></td>
-    <td><?php echo $data['telephone'] ?></td>
+        <td><?php echo $data['name'] ?></td>
+        <td><?php echo $data['age'] ?></td>
+        <td><?php echo $data['email'] ?></td>
+        <td><?php echo $data['telephone'] ?></td>
 
-    <td><button name="editData" id="editData"><a href="editData/index.php?id=<?php echo $data['id'];?>">Editar</button></td>
-    <td><button name="deletData" id="deleteData">Apagar</button></td>
+        <td><button name="editData" id="editData"><a href="editData/index.php?id=<?php echo $data['id'];?>">Editar</a></button></td>
+        <td><button name="deletData" id="deleteData"><a href="deletData/index.php?id=<?php echo $data['id'];?>">Apagar</a></button></td>
 
     </tr>
 
